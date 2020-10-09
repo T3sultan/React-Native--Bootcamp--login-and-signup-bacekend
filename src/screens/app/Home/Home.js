b4a63a31eeb7da5d35b90064f55e7bcc9e7cf310
T3sultan <60041747+T3sultan@users.noreply.github.com>
2020-10-09 11:00:48 -0700

    Add files via upload

---
+++ src/screens/app/Home/Home.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { View, Text } from 'react-native'
+import Button from '../../../components/common/Button'
+import { AuthContext } from '../../../context/AuthContext'
+
+export default function Home() {
+    const {authContext} = useContext(AuthContext);
+    const { signOut }= authContext;
+    return (
+        <View>
+            <Text>Home</Text>
+            <Button onPress={()=>{
+                signOut();
+            }} title="LOG OUT"/>
+        </View>
+    )
+}
